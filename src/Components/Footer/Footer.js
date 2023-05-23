e739e7b56ccdf39d8d0327708dc82beb08d4d838
Khaled Hegazy <khaledhegazy870@gmail.com>
2023-05-23 22:33:51 +0300

    Add Footer Links and Favicon icon

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -12,18 +12,13 @@ const Footer = () => {
         </div>
         <ul className={style.contactsList}>
           <li>
-            <a href="#">
+            <a href="https://www.facebook.com/m0tqn">
               <img src={facebookLogo} alt="Facebook Link" />
             </a>
           </li>
           <li>
-            <a href="#">
+            <a href="https://api.whatsapp.com/send?phone=%2B201025307327&data=AWD6UUKiqgkwTDKka_rN69z0RHtRw1JK41uXyPTV3YC4gf3hchlExkK8xPB11dAXZinZVNtOI4ooZzYFRAPNtQVJF0g0CufxvRMXeyzriDxUz3OGpUH_dBM4q-H0z-_BbBiXyrJ3C6LZpYRAhoLc46YEW92Ark7gOf8hXo3xRnSTVpEoVtVe9bI_q7b2jTP9vIbP5LxNwCpkbYtgONJ7viT-JBBmNVugQBsh17EkX_YT4t_97zIDxc3gyVUPbaJpCDWY0gcBxWMlirW1GFX-5rxBBxw-dxO5A010TYpD1jI8wyPyMXs&source=FB_Page&app=facebook&entry_point=page_cta&fbclid=IwAR1RoBLk-lwBgITvpbLOkEGMRSFBaVfgtoT9a3sMPJ2X_mgqQk9UPw95SjU">
               <img src={whatsappLogo} alt="Whatsapp Link" />
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <img src={instagramLogo} alt="Instagram Link" />
             </a>
           </li>
         </ul>
@@ -32,7 +27,9 @@ const Footer = () => {
         <h2>Got Questions</h2>
         <ul>
           <li>
-            <a href="#">+20 102 530 7327</a>
+            <a href="https://api.whatsapp.com/send?phone=%2B201025307327&data=AWD6UUKiqgkwTDKka_rN69z0RHtRw1JK41uXyPTV3YC4gf3hchlExkK8xPB11dAXZinZVNtOI4ooZzYFRAPNtQVJF0g0CufxvRMXeyzriDxUz3OGpUH_dBM4q-H0z-_BbBiXyrJ3C6LZpYRAhoLc46YEW92Ark7gOf8hXo3xRnSTVpEoVtVe9bI_q7b2jTP9vIbP5LxNwCpkbYtgONJ7viT-JBBmNVugQBsh17EkX_YT4t_97zIDxc3gyVUPbaJpCDWY0gcBxWMlirW1GFX-5rxBBxw-dxO5A010TYpD1jI8wyPyMXs&source=FB_Page&app=facebook&entry_point=page_cta&fbclid=IwAR1RoBLk-lwBgITvpbLOkEGMRSFBaVfgtoT9a3sMPJ2X_mgqQk9UPw95SjU">
+              +20 102 530 7327
+            </a>
           </li>
         </ul>
       </div>
